25482af92521b3c06489e6be24e5609b990f13bd
cristiano <cristiano.souza.mendonca@gmail.com>
2014-11-09 21:36:41 -0200

    Adding another version of Helpscout Notification

--- app/models/hotmart_notification.rb
+++ app/models/hotmart_notification.rb
@@ -3,7 +3,7 @@ class HotmartNotification < ActiveRecord::Base
   scope :find_latest_notifications_per_product,
     lambda { |user_id, customer_email|
       self.
-        where("user_id = #{user_id} AND customer_email = #{customer_email}").
+        where("user_id = #{user_id} AND customer_email = '#{customer_email}'").
         order("updated_at DESC").
         group_by { |notif| notif.prod_name }.
         map { |prod, notif| notif[0] }
