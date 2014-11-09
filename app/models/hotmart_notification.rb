1d8cb2744122f5aa15db9171d951f7acc4a4aa84
cristiano <cristiano.souza.mendonca@gmail.com>
2014-11-09 20:47:02 -0200

    Adding another version of Helpscout Notification

--- app/models/hotmart_notification.rb
+++ app/models/hotmart_notification.rb
@@ -1,15 +1,14 @@
 class HotmartNotification < ActiveRecord::Base
-  scope :find_all_by_user_id,
-          lambda { |user_id|
-            select(
-              "hotmart_transaction",
-              "phone_local_code",
-              "phone_number",
-              "prod_name",
-              "status",
-              "MAX(updated_at) AS updated_at").
-            where("user_id = #{user_id}").
-            group(:prod_name).
-            having('updated_at = MAX(updated_at)')
-          }
+
+  def self.find_all_by_user(user_id)
+    notifs = find_all_by_user_id(user_id)
+    grouped_notifs = notifs.group_by { |notif| notif.user_id }
+    grouped_notifs.try(:[],1)
+  end
+
+  private
+
+  def find_all_by_user_id(user_id)
+    self.where("user_id = #{user_id}")
+  end
 end
