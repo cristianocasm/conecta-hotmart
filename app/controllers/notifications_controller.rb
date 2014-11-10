f7914ac14cd40736ffafe809f1f6bb64eac41842
cristiano <cristiano.souza.mendonca@gmail.com>
2014-11-10 16:35:18 -0200

    Recording hotmart notification in hotmart_notifications table

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -6,11 +6,11 @@ class NotificationsController < ApplicationController
     user = User.find_by_hotmart_token_and_hottok(params[:token], params[:hottok]).try(:first)
     
     if user
+      record_notification(user.id)
+
       user.activation_rules.each do |regra|
         runActuationRules(user, regra) if regra.activated?(params)
       end
-
-      record_notification()
 
       render nothing: true, status: 200
     else
@@ -24,13 +24,48 @@ class NotificationsController < ApplicationController
     rule.actuation_rules.each { |act_rule| act_rule.run(user, params) }
   end
 
-  def record_notification
+  def record_notification(user_id)
     HotmartNotification.create({
-      transaction: params[:transaction],
+      hotmart_transaction: params[:transaction],
       phone_local_code: params[:phone_local_code],
       phone_number: params[:phone_number],
       prod_name: params[:prod_name],
-      status: params[:status]
+      status: params[:status],      
+      user_id: user_id,
+      customer_email: params[:email],
+      hottok: params[:hottok],
+      prod: params[:prod],
+      off: params[:off],
+      price: params[:price],
+      aff: params[:aff],
+      aff_name: params[:aff_name],
+      name: params[:name],
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      doc: params[:doc],
+      address: params[:address],
+      address_number: params[:address_number],
+      address_country: params[:address_country],
+      address_district: params[:address_district],
+      address_comp: params[:address_comp],
+      address_city: params[:address_city],
+      address_state: params[:address_state],
+      address_zip_code: params[:address_zip_code],
+      xcod: params[:xcod],
+      src: params[:src],
+      payment_engine: params[:payment_engine],
+      payment_type: params[:payment_type],
+      hotkey: params[:hotkey],
+      name_subscription_plan: params[:name_subscription_plan],
+      subscriber_code: params[:subscriber_code],
+      cms_marketplace: params[:cms_marketplace],
+      cms_vendor: params[:cms_vendor],
+      cms_aff: params[:cms_aff],
+      callback_type: params[:callback_type],
+      subscription_status: params[:subscription_status],
+      transaction_ext: params[:transaction_ext],
+      sck: params[:sck],
+      purchase_date: params[:purchase_date]
       })
   end
 end
